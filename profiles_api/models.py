9c0471d72f319ff1d5d2a3e2b9d7be8b174830b9
Shashwat Singh <shashwatsingh741@gmail.com>
2022-11-09 23:51:39 +0530

    models.py bug resolved

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -33,7 +33,7 @@ class UserProfileManager(BaseUserManager):
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
     email = models.EmailField(max_lenght=255, unique=True)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_lenght=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
