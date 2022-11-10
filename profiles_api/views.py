015a7d66753bfe16c4b6c368ec36c61a6e4959d5
Shashwat Singh <shashwatsingh741@gmail.com>
2022-11-10 09:57:53 +0530

    HelloView setup for api testing

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+    def get(self, request, format = None):
+        an_apiview = [
+            'Uses HTTP method as function',
+            'Is similar to traditional Django View'   
+            'Gives you most control over your application logic'
+        ]
+        return Response({'message' : 'Hello!', 'an_apiview' : an_apiview})
