0738c4c7f6e3574dc6d1e56649f96e15afdfc30f
Shashwat Singh <shashwatsingh741@gmail.com>
2022-11-10 23:46:44 +0530

    HelloViewSet class created and list function defined

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from profiles_api.views import *
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('hello-viewset', HelloViewSet, basename='hello-viewset')
 
 urlpatterns = [
-    path('hello-view/', HelloApiView.as_view())
+    path('hello-view/', HelloApiView.as_view()),
+    path('', include(router.urls))
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from profiles_api import serializers
+from rest_framework import viewsets
 
 class HelloApiView(APIView):
     serializer_class = serializers.HelloSerializer
@@ -35,3 +36,13 @@ class HelloApiView(APIView):
 
     def delete(self, request, pk=None):
         return Response({'method' : 'DELETE'})
+
+class HelloViewSet(viewsets.ViewSet):
+    def list(self, request):
+        a_viewset = [
+            'Uses actions (list, create, retrieve, update, partial_update)',
+            'Automatically maps to URLs using Routers',
+            'Provides more functionality with less code'
+        ]
+
+        return Response({'message' : 'Hello!', 'a_viewset' : a_viewset})
