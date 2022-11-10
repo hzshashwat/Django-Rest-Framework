8840f74b8dbcfa004cce8647078b6cd9cdf2d3c0
Shashwat Singh <shashwatsingh741@gmail.com>
2022-11-11 00:24:50 +0530

    Added viewsets to profile api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -38,6 +38,7 @@ class HelloApiView(APIView):
         return Response({'method' : 'DELETE'})
 
 class HelloViewSet(viewsets.ViewSet):
+    serializer_class = serializers.HelloSerializer
     def list(self, request):
         a_viewset = [
             'Uses actions (list, create, retrieve, update, partial_update)',
@@ -46,3 +47,28 @@ class HelloViewSet(viewsets.ViewSet):
         ]
 
         return Response({'message' : 'Hello!', 'a_viewset' : a_viewset})
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message' : message})
+        else:
+            return Response(
+                serializer.errors,
+                status= status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self, request, pk=None):
+        return Response({'http_method' : 'GET'})
+
+    def update(self, request, pk=None):
+        return Response({'http_method' : 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        return Response({'http_method' : 'PATCH'})
+
+    def destroy(self, request, pk = None):
+        return Response({'http_method' : 'DELETE'})
